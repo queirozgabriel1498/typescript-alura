244f6d46841b7bbe8a1a38e6f4cb06335123cba2
Gabriel Queiroz <gabriel.queiroz@polichat.com.br>
2022-09-23 11:45:19 -0300

    change array type method

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,16 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-  private negociacoes: Array<Negociacao> = [];
+  private negociacoes: Negociacao[] = [];
 
   adiciona(negociacao: Negociacao) {
     this.negociacoes.push(negociacao);
   }
 
-  lista(): ReadonlyArray<Negociacao> {
+  lista(): readonly Negociacao[] {
     return [...this.negociacoes];
   }
 }
+
+// Array<Negociacao> = Negociacao[]
+// ReadonlyArray<Negociacao> = readonly Negociacao[]
